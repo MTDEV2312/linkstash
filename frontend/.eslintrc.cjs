54608d7d088288a053767a6da4644665df23a008
Mathias Teran <agusmaty23@gmail.com>
2025-10-31 15:42:28 -0500

    feat: Añadir configuración de ESLint y dependencias necesarias

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    react: { version: 'detect' }
+  },
+  ignorePatterns: ['dist/', 'build/', 'node_modules/']
+}
---
+++ frontend/eslint.config.js
@@ -0,0 +1,3 @@
+// Empty placeholder config to avoid eslint attempting to import 'eslint/config'
+// The real configuration for this project is provided in .eslintrc.cjs
+export default {}
